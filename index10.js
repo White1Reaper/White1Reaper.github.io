8d2744535de171010e06008fe6caa1182df9d917
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-03 10:29:32 +0300

    Update index10.js

--- index10.js
+++ index10.js
@@ -1,15 +1,76 @@
-window.addEventListener('DOMContentLoaded', function (event) {
+      function click1() {
+let f1 =document.querySelector(".field1");
+ let r = document.getElementById("result");
+let select = document.querySelector('.myselect1');
+
+         let c1 = document.getElementById("c1");
+        let c2 = document.getElementById("c2");
+const radio1 = document.querySelector('input[name="myradio"]:checked')
+
+let b=1*f1.value;        
+        if(select.value=="1"){
+          b=b*10;
+        }
+else {
+                if(select.value=="2"){
+                  
+
+if(radio1.value=="1"){
+
+b=b*15;
+}
+                  else{
+if(radio1.value=="2"){
+           
+                    b=b*50;
+                  }
+                    else {
+ if(radio1.value=="3"){
+                  
+                    b=b*500;
+                  }
+                    }
+        }
+                }
+  else{
+    if(select.value=="3"){
+                  b=b*1000000;
+                  if(c2.checked){
+                  b=b*15;
+                  }
+                                    if(c1.checked){
+                  b=b+15000;
+                  }
+        }
+    
+  }
+        }
+r.innerHTML=b;
+return false;
+
+}
+    window.addEventListener('DOMContentLoaded', function (event) {
  let s = document.getElementsByName("myselect");
+ let radios = document.getElementById("myradios");
+radios.style.display = "none";
+let checkboxes = document.getElementById("checkboxes");
+ checkboxes.style.display = "none";     
  s[0].addEventListener("change", function(event) {
  let select = event.target;
- let radios = document.getElementById("myradios");
- console.log(select.value);
- // Можно использовать getElementsByClassName()
+ radios.style.display = "none";
  if (select.value == "1") {
  radios.style.display = "none";
+   checkboxes.style.display = "none";     
  }
  else {
+   if(select.value == "2"){
  radios.style.display = "block";
+ checkboxes.style.display = "none";
+   }
+   else {
+      checkboxes.style.display = "block";
+      radios.style.display = "none";
+   }
  }
  });
 
@@ -22,4 +83,3 @@ window.addEventListener('DOMContentLoaded', function (event) {
  });
 
 });
-
