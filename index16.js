d167df0752e45d7b1a7e9fb8aa0932575dc35d87
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-12 16:52:57 +0300

    Update index16.js

--- index16.js
+++ index16.js
@@ -1,10 +1,4 @@
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
- function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-} 
 document.getElementById("clearButton").onclick = function(e) {
   document.getElementByName("message").value = "";
       document.getElementByName("f-name").value = "";
