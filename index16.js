0ca9a8b616ff56e04f7eff91c9d373710b80b5e7
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-13 15:28:18 +0300

    Update index16.js

--- index16.js
+++ index16.js
@@ -9,6 +9,11 @@
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
+ var state = { 'page_id': 1, 'user_id': 5 };
+var title = 'Hello World';
+var url = 'hello-world.html';
+
+history.pushState(state, title, url);
 }
  function closeForm() {
     document.getElementById("myForm").style.display = "none";
