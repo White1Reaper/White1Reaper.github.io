adcf80f4bf4dd52d5a1a75dff228c463a91bc7aa
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-13 20:35:50 +0300

    Update index16.js

--- index16.js
+++ index16.js
@@ -1,17 +1,16 @@
 		document.addEventListener('DOMContentLoaded', function () {
+			      var inputName= document.getElementById("1");
+ inputName.value=localStorage.getItem('Name')
       var inputEmail= document.getElementById("2");
  inputEmail.value=localStorage.getItem('email')
+			      var inputTextarea= document.getElementById("3");
+ inputTextarea.value=localStorage.getItem('Textarea')
 })
- function clear() {
+ function send() {
   document.getElementById("1").value = "";
       document.getElementById("2").value = "";
   document.getElementById("3").value = "";
-  var state = { 'page_id': 1, 'user_id': 5 };
-var title = 'Hello World';
-var url = 'index14.html';
 
- history.replaceState(null, null, 'index14.html');
-history.pushState(null, null, 'index14.html');
 
     document.getElementById("myForm").style.display = "block";
  }
@@ -23,7 +22,6 @@ var title = 'Hello World';
 var url = 'hello-world.html';
 history.pushState(state, title, url);
 	  var inputEmail= document.getElementById("2");
-while (1){
 	localStorage.setItem("email", inputEmail.value);
 }
  function closeForm() {
@@ -34,3 +32,11 @@ var url = 'index14.html';
  history.replaceState(null, null, url);
 history.pushState(null, null, url);
  } 
+function saveForm(){
+		  var inputName= document.getElementById("1");
+	localStorage.setItem("Name", inputName.value);
+		  var inputEmail= document.getElementById("2");
+	localStorage.setItem("email", inputEmail.value);
+		  var inputTextarea= document.getElementById("3");
+	localStorage.setItem("Textarea", inputTextarea.value);
+}
