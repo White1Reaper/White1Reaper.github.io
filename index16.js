99851ae531f7ba040505ef02104f7485c45c4df9
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-14 09:50:54 +0300

    Update index16.js

--- index16.js
+++ index16.js
@@ -19,6 +19,12 @@ function openForm() {
 }
  function closeForm() {
     document.getElementById("myForm").style.display = "none";
+	 		  var inputName= document.getElementById("1");
+	localStorage.setItem("name", inputName.value);
+		  var inputEmail= document.getElementById("2");
+	localStorage.setItem("email", inputEmail.value);
+		  var inputTextarea= document.getElementById("3");
+	localStorage.setItem("textarea", inputTextarea.value);
  } 
 function saveForm(){
 		  var inputName= document.getElementById("1");
