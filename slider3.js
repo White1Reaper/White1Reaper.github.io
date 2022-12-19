fb713030c4cec512075a83ff8e29075cbde93123
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-19 15:49:51 +0300

    Update slider3.js

--- slider3.js
+++ slider3.js
@@ -1,7 +1,22 @@
-$(".single-item").slick({
+		document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM готов!')
+})
+		const mediaQuery = window.matchMedia('(min-width: 1000px)');
+		if (mediaQuery.matches) {
+    $(".multiple-items").slick({
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToShow: 4,
+  slidesToScroll: 4
 });
+		}
+		else{
+		    $(".multiple-items").slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
+    }
