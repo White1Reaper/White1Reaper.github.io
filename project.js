44d0bad5349c1d923a8b531a56d20290747a68c8
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2022-12-24 19:28:35 +0300

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,56 @@
 
+// Авто-прокрутка слайдера 1
+
+$(function(){
+    $(".slayder_body1").jCarouselLite({
+		circular: true,
+		visible: 7,
+        auto: 2000,
+        speed: 1000
+    });
+});
+
+// Авто-прокрутка слайдера 2
+$(function(){
+    $(".slayder_body2").jCarouselLite({
+		circular: true,
+		visible: 8,
+        auto: 2000,
+        speed: 1000
+    });
+});
+
+
+
+
+
+/* 8. Блок - Отзывы */
+$(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+
+
+
+$("#send1").click(function () {
+    
+
+    var slapform = new Slapform();
+    $("#send1").prop("disabled", true);
+    slapform.submit({
+        form: "vog0f58Vj"
+        }).then(function () {
+            alert("successful sending!");
+        }).catch(function () {
+            alert("sending failed");
+        });
+    document.getElementById("1").value = "";
+    document.getElementById("2").value = "";
+    document.getElementById("3").value = "";
+    document.getElementById("4").value = "";
+    document.querySelector("#happy").checked = false;
+    return false;
+});
